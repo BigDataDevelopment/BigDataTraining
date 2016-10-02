52a79e5852056a7b414290d169ed264f02adfc00
abhinandante <abhinandan@thirdeyecss.com>
2016-10-02 07:59:21 +0530

    add custom pertitioner, mapsidejoin & Mapreduce using Python

---
+++ src/main/java/com/hadoop/partitioner/Partitioner.java
@@ -0,0 +1,9 @@
+package com.hadoop.partitioner;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+
+public class Partitioner extends MapReduceBase  {
+
+}
